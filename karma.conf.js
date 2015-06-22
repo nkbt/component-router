62f3f66844352ff295c625413afa1e28443ea9c1
Nik Butenko <nik@butenko.me>
2015-06-23 00:37:30 +1000

    Add Karma, coverage and base test

---
+++ karma.conf.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+var path = require('path');
+
+
+var webpackConfig = {
+  devtool: 'eval',
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  module: {
+    preLoaders: [
+      {test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'test')},
+      {test: /\.js$/, loader: 'isparta', include: path.join(__dirname, 'src')}
+    ]
+  },
+
+  stats: {
+    colors: true
+  }
+};
+
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'node_modules/babel-core/browser-polyfill.js',
+      'test/**/*.js'
+    ],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      quiet: true
+    },
+    exclude: [],
+    preprocessors: {
+      'test/**/*.js': ['webpack']
+    },
+    reporters: ['progress'],
+    coverageReporter: {
+      dir: './coverage/',
+      subdir: function (browser) {
+        return browser.toLowerCase().split(/[ /-]/)[0];
+      },
+      reporters: [
+        {type: 'text', file: 'text.txt'},
+        {type: 'text-summary', file: 'text-summary.txt'},
+        {type: 'html'}
+      ]
+    },
+    captureTimeout: 90000,
+    browserNoActivityTimeout: 60000,
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true
+  });
+};
---
+++ test/UrlUtil.js
@@ -0,0 +1,9 @@
+describe('UrlUtil', () => {
+  const UrlUtil = require('inject!../src/UrlUtil')({
+  });
+
+
+  it('should be ok', () => {
+    expect(UrlUtil).toBeTruthy();
+  });
+});
