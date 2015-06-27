c9c8d48eb25f8a9ded1898b0a07a8b1f1c86c2d2
Nik Butenko <nik@butenko.me>
2015-06-27 19:52:19 +1000

    Mock all internal dependencies and spy on `window` methods

--- test/Location/Html4-test.js
+++ test/Location/Html4-test.js
@@ -1,11 +1,19 @@
 describe('Html4', () => {
   const Html4Injector = require('inject!../../src/Location/Html4');
   let Store, ActionCreator, Html4;
+  let storeUnsubscribe;
 
 
   beforeEach(() => {
-    Store = jasmine.createSpyObj('Store', ['']);
-    ActionCreator = jasmine.createSpyObj('ActionCreator', ['']);
+    storeUnsubscribe = jasmine.createSpy('storeUnsubscribe');
+    Store = jasmine.createSpyObj('Store', ['addChangeListener', 'getCleanQuery', 'getPathname']);
+    Store.addChangeListener.and.returnValue(storeUnsubscribe);
+
+    ActionCreator = jasmine.createSpyObj('ActionCreator', ['restoreLocation']);
+
+    spyOn(window, 'addEventListener').and.returnValue(false);
+    spyOn(window, 'removeEventListener').and.returnValue(false);
+    spyOn(window.history, 'pushState').and.returnValue(false);
   });
 
 
