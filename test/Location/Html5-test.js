fe520e95cd9367f26f6c747a1577243400466a6f
Nik Butenko <nik@butenko.me>
2015-06-29 08:41:19 +1000

    Cover Location/Html5

--- test/Location/Html5-test.js
+++ test/Location/Html5-test.js
@@ -1,11 +1,29 @@
-describe('Html5', () => {
-  const Html5Injector = require('inject!../../src/Location/Html5');
-  let Store, ActionCreator, Html5;
+import React from 'react';
+import createComponent from '../utils/createComponent';
+
+
+const TestUtils = React.addons.TestUtils;
+const w = global.mockedWindow;
+
+
+describe('LocationHtml5', () => {
+  const Html5Injector = require('imports?window=>mockedWindow!inject!../../src/Location/Html5');
+  let Store, ActionCreator, Html5, div;
+  let storeUnsubscribe;
 
 
   beforeEach(() => {
-    Store = jasmine.createSpyObj('Store', ['']);
-    ActionCreator = jasmine.createSpyObj('ActionCreator', ['']);
+    storeUnsubscribe = jasmine.createSpy('storeUnsubscribe');
+    Store = jasmine.createSpyObj('Store', ['addChangeListener', 'getCleanQuery', 'getPathname']);
+    Store.addChangeListener.and.returnValue(storeUnsubscribe);
+
+    ActionCreator = jasmine.createSpyObj('ActionCreator', ['restoreLocation']);
+
+    w.location = {pathname: '/', search: ''};
+    w.addEventListener = jasmine.createSpy('addEventListener');
+    w.removeEventListener = jasmine.createSpy('removeEventListener');
+    w.history = jasmine.createSpyObj('history', ['pushState']);
+    w.document = {title: 'title'};
   });
 
 
@@ -15,7 +33,114 @@ describe('Html5', () => {
   }));
 
 
-  it('should be ok', () => {
-    expect(Html5).toBeTruthy();
+  it('should not render anything', () => {
+    const {out} = createComponent(Html5);
+    expect(out).toBeNull();
+  });
+
+
+  describe('Lifecycle', () => {
+    beforeEach(() => {
+      div = document.createElement('div');
+      document.documentElement.appendChild(div);
+      React.render(<Html5 />, div);
+    });
+
+    it('should restore url on load', () => {
+      expect(ActionCreator.restoreLocation).toHaveBeenCalled();
+    });
+
+
+    it('should subscribe to location change', () => {
+      expect(w.addEventListener).toHaveBeenCalled();
+      expect(w.addEventListener.calls.mostRecent().args[0]).toEqual('popstate');
+    });
+
+
+    it('should subscribe to Store changes', () => {
+      expect(Store.addChangeListener).toHaveBeenCalled();
+    });
+
+
+    it('should unsubscribe from Store changes when unmounted', () => {
+      React.unmountComponentAtNode(div);
+      expect(storeUnsubscribe).toHaveBeenCalled();
+    });
+
+
+    it('should unsubscribe from location change', () => {
+      React.unmountComponentAtNode(div);
+      expect(w.removeEventListener).toHaveBeenCalled();
+      expect(w.removeEventListener.calls.mostRecent().args[0]).toEqual('popstate');
+    });
+  });
+
+
+  describe('URL manipulations', () => {
+    let html5;
+
+    beforeEach(() => html5 = TestUtils.renderIntoDocument(<Html5 />));
+
+
+    it('should simplify url to only pathname and query', () => {
+      w.location.port = 1234;
+      w.location.pathname = '/test';
+      w.location.search = '?x=1';
+      w.location.hash = '#anchor';
+      expect(html5.getUrl()).toEqual('/test?x=1');
+    });
+
+
+    it('should set url pushing new state to history', () => {
+      html5.setUrl('/test?x=1', {}, 'OMG!');
+      expect(w.history.pushState).toHaveBeenCalled();
+      expect(w.history.pushState).toHaveBeenCalledWith({}, 'OMG!', '/test?x=1');
+    });
+
+
+    it('should use current window title if one not provided', () => {
+      w.document.title = 'OMG!';
+      html5.setUrl('/test?x=1', {});
+      expect(w.history.pushState).toHaveBeenCalledWith({}, 'OMG!', '/test?x=1');
+    });
+
+
+    it('should use default empty state if one not provided', () => {
+      w.document.title = 'OMG!';
+      html5.setUrl('/test?x=1');
+      expect(w.history.pushState).toHaveBeenCalled();
+      expect(w.history.pushState).toHaveBeenCalledWith({}, 'OMG!', '/test?x=1');
+    });
+
+
+    it('should not push new state if url is the same', () => {
+      w.location.pathname = '/test';
+      w.location.search = '?x=1';
+      html5.setUrl('/test?x=1');
+      expect(w.history.pushState).not.toHaveBeenCalled();
+    });
+
+
+    it('should restore url when location updated', () => {
+      w.location.pathname = '/test';
+      w.location.search = '?x=1';
+
+      const onPopstate = w.addEventListener.calls.mostRecent().args[1];
+      onPopstate();
+
+      expect(ActionCreator.restoreLocation).toHaveBeenCalled();
+      expect(ActionCreator.restoreLocation).toHaveBeenCalledWith({location: '/test?x=1'});
+    });
+
+
+    it('should set url when store updated', () => {
+      spyOn(html5, 'setUrl');
+      Store.getPathname.and.returnValue('/test');
+      Store.getCleanQuery.and.returnValue({x: 1, y: 2});
+      const onChange = Store.addChangeListener.calls.mostRecent().args[0];
+      onChange();
+      expect(html5.setUrl).toHaveBeenCalled();
+      expect(html5.setUrl).toHaveBeenCalledWith('/test?x=1&y=2');
+    });
   });
 });
