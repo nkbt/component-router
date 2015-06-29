673411daaf04af5c9df342240f65c85c5d734b8d
Nik Butenko <nik@butenko.me>
2015-06-29 23:55:23 +1000

    Quickstart and Foobar examples

---
+++ src/example/FooBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {InFlux, Url, LocationHtml4} from '..';
+
+const Foo = React.createClass({
+  render() {
+    return <h1>Foo</h1>;
+  }
+});
+
+const Bar = React.createClass({
+  render() {
+    return <h1>Bar</h1>;
+  }
+});
+
+const Baz = React.createClass({
+  propTypes: {
+    inFlux: React.PropTypes.object
+  },
+
+  render() {
+    const {Component} = this.props.inFlux;
+    return <Component />;
+  }
+});
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <LocationHtml4 />
+        <Url query={{baz: 'foo'}}>Foo</Url>
+        &nbsp;|&nbsp;
+        <Url query={{baz: 'bar'}}>Bar</Url>
+        <InFlux config={{foo: Foo, bar: Bar}} namespace="baz">
+          <Baz />
+        </InFlux>
+      </div>
+    );
+  }
+});
+
+React.render(<App />, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 var env = process.env.NODE_ENV || 'production';
+var entry = process.env.ENTRY || 'Example';
 
 
 var eslintLoader = {
@@ -16,7 +17,7 @@ module.exports = {
   devtool: 'eval',
 
   entry: [
-    './src/example/Example.js'
+    './src/example/' + entry + '.js'
   ].concat(env === 'production' ? [] : [
       'webpack-dev-server/client?http://localhost:8080',
       'webpack/hot/only-dev-server'
