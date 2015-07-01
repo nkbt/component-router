d96c397e1b528e0ede94b5b541ffbf8539ab11e7
Nik Butenko <nik@butenko.me>
2015-07-01 23:03:27 +1000

    Remove params from query

--- test/Store-test.js
+++ test/Store-test.js
@@ -19,28 +19,27 @@ describe('Store', () => {
   });
 
 
-  const createStore = () => Store = StoreInjector({
-    'sorted-object': sorted,
-    './Constants': Constants,
-    './Dispatcher': Dispatcher,
-    './urlUtil': urlUtil
-  });
-
-
-  beforeEach(createStore);
-
-
-  beforeEach(() => reaction = Dispatcher.register.calls.mostRecent().args[0]);
+  const createStore = () => {
+    Store = StoreInjector({
+      'sorted-object': sorted,
+      './Constants': Constants,
+      './Dispatcher': Dispatcher,
+      './urlUtil': urlUtil
+    });
+    reaction = Dispatcher.register.calls.mostRecent().args[0];
+  };
 
 
   describe('Init', () => {
     it('should initialize with current url data', () => {
+      createStore();
       expect(Store.getQuery()).toEqual({x: 1});
       expect(Store.getPathname()).toEqual('/test');
     });
 
 
     it('should have no default params', () => {
+      createStore();
       expect(Store.getDefaultParams()).toEqual({});
     });
 
@@ -54,19 +53,32 @@ describe('Store', () => {
   });
 
 
-  it('should restore location', () => {
-    urlUtil.parseHref.and.returnValue({pathname: '/hello', query: {world: 123}});
-    reaction({
-      actionType: Constants.RESTORE_LOCATION,
-      payload: {location: '/hello?world=123'}
+  describe('Restore location', () => {
+    beforeEach(() => {
+      createStore();
+      urlUtil.parseHref.and.returnValue({pathname: '/hello', query: {world: 123}});
+      reaction({
+        actionType: Constants.RESTORE_LOCATION,
+        payload: {location: '/hello?world=123'}
+      });
     });
 
-    expect(Store.getPathname()).toEqual('/hello');
-    expect(Store.getQuery()).toEqual({world: 123});
+
+    it('should restore location', () => {
+      expect(Store.getPathname()).toEqual('/hello');
+      expect(Store.getQuery()).toEqual({world: 123});
+    });
+
+
+    it('should sort query params when restoring location', () => {
+      expect(sorted.calls.mostRecent().args).toEqual([{world: 123}]);
+    });
   });
+
 
   describe('Default params', () => {
     beforeEach(() => {
+      createStore();
       reaction({
         actionType: Constants.ADD_DEFAULT_PARAM,
         payload: {namespace: 'y', value: 10}
@@ -88,12 +100,49 @@ describe('Store', () => {
     });
 
 
-    it('should not override current query with default param', () => {
+    it('should not override existing query param with default one', () => {
       reaction({
         actionType: Constants.ADD_DEFAULT_PARAM,
         payload: {namespace: 'x', value: 123}
       });
       expect(Store.getQuery()).toEqual({x: 1, y: 10, z: 100});
+    });
+
+
+    it('should sort query params when adding default one', () => {
+      expect(sorted.calls.mostRecent().args).toEqual([{x: 1, y: 10, z: 100}]);
+    });
+  });
+
+
+  describe('Remove param', () => {
+    beforeEach(() => {
+      urlUtil.parseHref.and.returnValue({query: {x: 1, y: 10}});
+      createStore();
+      reaction({
+        actionType: Constants.ADD_DEFAULT_PARAM,
+        payload: {namespace: 'z', value: 100}
+      });
+    });
+
+
+    it('should remove param from query', () => {
+      expect(Store.getQuery()).toEqual({x: 1, y: 10, z: 100});
+      reaction({actionType: Constants.REMOVE_PARAM, payload: {namespace: 'x'}});
+      expect(Store.getQuery()).toEqual({y: 10, z: 100});
+    });
+
+
+    it('should remove param from default params', () => {
+      expect(Store.getDefaultParams()).toEqual({z: 100});
+      reaction({actionType: Constants.REMOVE_PARAM, payload: {namespace: 'z'}});
+      expect(Store.getDefaultParams()).toEqual({});
+    });
+
+
+    it('should sort query params upon removal', () => {
+      reaction({actionType: Constants.REMOVE_PARAM, payload: {namespace: 'x'}});
+      expect(sorted.calls.mostRecent().args).toEqual([{y: 10, z: 100}]);
     });
   });
 });
