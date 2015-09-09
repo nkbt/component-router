e1813192a31f86e9b4708dacf75f771341b35d92
Hal Henke <halhenke@gmail.com>
2015-09-10 00:12:54 +1000

    Kinda ugly but tests for main cases of ComponentRouter.render

--- test/ComponentRouter-test.js
+++ test/ComponentRouter-test.js
@@ -1,5 +1,4 @@
-import React from 'react';
-// import React from 'react/addons';
+import React from 'react/addons';
 import createComponent from './utils/createComponent';
 import FooBar from '../src/example/FooBar/FooBar';
 
@@ -8,11 +7,13 @@ describe('ComponentRouter', () => {
   const ComponentRouterInjector = require('inject!../src/ComponentRouter');
   let Actions, Store, getDefault, ComponentRouter;
 
-  const ChartInjector = require('inject!../src/example/Quickstart/Chart');
-  const Chart = ChartInjector({
-    './Chart.css': jasmine.createSpyObj('styles', ['']),
+  const Chart = React.createClass({
+    render: function() {
+      return (
+        <div />
+      );
+    }
   });
-
 
   beforeEach(() => {
     // Actions = jasmine.createSpyObj('Actions', ['']);
@@ -29,57 +30,67 @@ describe('ComponentRouter', () => {
   });
 
 
-  beforeEach(() => ComponentRouter = ComponentRouterInjector({
-    './Actions': Actions,
-    './Store': Store,
-    './getDefault': getDefault
-  }));
-  // beforeEach(() => {
-  //   storeUnsubscribe = jasmine.createSpy('storeUnsubscribe');
-  //   // const initState = {
-  //   //   query: {},
-  //   //   defaultParams: {},
-  //   //   type: Constants.TYPE_HTML5
-  //   // };
-  //   Store = jasmine.createSpyObj('Store', [
-  //     'addThrottledChangeListener', 'dispatch', 'getQuery', 'getDefaultParams']);
-  //   Store.addThrottledChangeListener.and.returnValue(storeUnsubscribe);
-  //   Store.getQuery.and.returnValue({});
-  //   initProps = {
-  //     namespace: 'page',
-  //     config: {}
-  //   };
-  //   ComponentRouter = ComponentRouterInjector({
-  //     './ActionCreator': ActionCreator,
-  //     './Store': Store,
-  //     './getDefault': getDefault
-  //   });
-  // });
+  // beforeEach(() => ComponentRouter = ComponentRouterInjector({
+  //   './Actions': Actions,
+  //   './Store': Store,
+  //   './getDefault': getDefault
+  // }));
+  // // beforeEach(() => {
+  // //   storeUnsubscribe = jasmine.createSpy('storeUnsubscribe');
+  // //   // const initState = {
+  // //   //   query: {},
+  // //   //   defaultParams: {},
+  // //   //   type: Constants.TYPE_HTML5
+  // //   // };
+  // //   Store = jasmine.createSpyObj('Store', [
+  // //     'addThrottledChangeListener', 'dispatch', 'getQuery', 'getDefaultParams']);
+  // //   Store.addThrottledChangeListener.and.returnValue(storeUnsubscribe);
+  // //   Store.getQuery.and.returnValue({});
+  // //   initProps = {
+  // //     namespace: 'page',
+  // //     config: {}
+  // //   };
+  // //   ComponentRouter = ComponentRouterInjector({
+  // //     './ActionCreator': ActionCreator,
+  // //     './Store': Store,
+  // //     './getDefault': getDefault
+  // //   });
+  // // });
+  beforeEach(() => {
+    storeUnsubscribe = jasmine.createSpy('storeUnsubscribe');
+    Store = jasmine.createSpyObj('Store', [
+      'addThrottledChangeListener', 'dispatch', 'getQuery', 'getDefaultParams']);
+    Store.addThrottledChangeListener.and.returnValue(storeUnsubscribe);
+    Store.getQuery.and.returnValue({});
+    initProps = {
+      namespace: 'page',
+      config: {}
+    };
+    ComponentRouter = ComponentRouterInjector({
+      './Actions': Actions,
+      './Store': Store,
+      './getDefault': getDefault
+    });
+  });
+
 
   it('should be ok', () => {
     expect(ComponentRouter).toBeTruthy();
   });
 
-  // it('should not render anything', () => {
-  //   const {out} = createComponent(ComponentRouter, initProps);
-  //
-  //   expect(out).toBeNull();
-  // });
   describe('Lifecycle', () => {
+    let comp;
     let div;
     let mock;
 
     beforeEach(() => {
       div = document.createElement('div');
       React.render(<ComponentRouter {...initProps}/>, div);
-      // mock = React.addons.TestUtils.mockComponent(ComponentRouter);
       mock = React.addons.TestUtils.renderIntoDocument(<ComponentRouter {...initProps} />);
     });
 
     it('should add a listener on mount', () => {
       expect(Store.addThrottledChangeListener).toHaveBeenCalled();
-      // const {out} = createComponent(ComponentRouter, initProps);
-      // expect(Store.addThrottledChangeListener.calls.mostRecent().args[0]).toEqual(createComponent(ComponentRouter, initProps).onChange);
       expect(Store.addThrottledChangeListener.calls.mostRecent().args[0]).toEqual(mock.onChange);
       expect(Store.addThrottledChangeListener.calls.mostRecent().args[1]).toEqual(50);
     });
@@ -126,38 +137,56 @@ describe('ComponentRouter', () => {
     describe("when passed a React Component as config", function () {
       describe("without any children", function () {
         it("should render that Component", function () {
-          // const config = <FooBar />;
-          const cr = createComponent(ComponentRouter, {namespace:'zoo',config: FooBar}).out;
-          // const cr = createComponent(ComponentRouter, namespace='zoo' {config: FooBar}).out;
+          const namespace = {namespace: 'zoo'};
+          const config = {config: FooBar};
+          const cr = createComponent(ComponentRouter, {...namespace, ...config}).out;
           expect(cr.type).toBe(<FooBar />.type);
-          // expect(cr.type).toBe('booga');
-          // expect(cr).toBe('booga');
         });
       });
+
       describe("and it has children", function () {
         it("should render the children", function () {
-          Store.getQuery.and.returnValue({page: 'foo'});
-          const cr = createComponent(ComponentRouter, {namespace: 'page', config: {page: Chart, foo: FooBar}}, <div></div>).out;
-          expect(cr.props.children).toEqual(<div></div>);
+          // Store.getQuery.and.returnValue({page: 'foo'});
+          const config = {config: FooBar};
+          const namespace = {namespace: 'page'};
+          const cr = createComponent(ComponentRouter, {...namespace, ...config}, <div></div>).out;
+          expect(cr.type).toEqual('div');
+          expect(cr.props.componentRouter.namespace).toEqual('page');
+          expect(cr.props.componentRouter.value).toBeUndefined();
+          expect(cr.props.componentRouter.config).toEqual({});
+          expect(cr.props.componentRouter.keys).toEqual([]);
+          expect(cr.props.componentRouter.Component).toEqual(FooBar);
         });
       });
     });
+
     describe("when passed an object as config", function () {
       describe("without any children", function () {
         it("should render that Component", function () {
           Store.getQuery.and.returnValue({page: 'foo'});
-          const cr = createComponent(ComponentRouter, {namespace: 'page', config: {foo: Chart, bar: FooBar}}).out;
+          const namespace = {namespace: 'page'};
+          const config = {foo: Chart, bar: FooBar};
+          const cr = createComponent(ComponentRouter, {...namespace, config}).out;
           expect(cr.type).toBe(<Chart />.type);
         });
       });
+
       describe("and it has children", function () {
         it("should render the children", function () {
-
+          Store.getQuery.and.returnValue({page: 'foo'});
+          const config = {page: Chart, foo: FooBar};
+          const namespace = {namespace: 'page'};
+          const cr = createComponent(ComponentRouter, {...namespace, config}, <div></div>).out;
+          expect(cr.type).toEqual('div');
+          expect(cr.props.componentRouter.namespace).toEqual('page');
+          expect(cr.props.componentRouter.value).toEqual('foo');
+          expect(cr.props.componentRouter.config).toEqual(config);
+          expect(cr.props.componentRouter.keys).toEqual(Object.keys(config));
+          expect(cr.props.componentRouter.Component).toEqual(FooBar);
         });
+
       });
     });
   });
-
   });
-
 });
