d8617a24c3c1d84d07f84b321b239e52d57b55b1
Nik Butenko <nik@butenko.me>
2015-11-10 13:58:51 +1100

    Fix module export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import {navigateTo, restoreLocation, addDefaultParam, removeParam} from './Actions';
+import {navigateTo, restoreLocation, addDefaultParam, removeParam} from './actions';
 
-export {default as ComponentRouter} from './ComponentRouter';
-export {default as Store} from './Store';
+export {componentRouter} from './reducer';
 export {default as Constants} from './Constants';
-export {default as getDefault} from './getDefault';
-export const Actions = {navigateTo, restoreLocation, addDefaultParam, removeParam};
+export const actions = {navigateTo, restoreLocation, addDefaultParam, removeParam};
