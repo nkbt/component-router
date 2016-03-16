03da3991423f41ba722b0be54a003c0b6ea1ebcc
Nik Butenko <nik@butenko.me>
2016-03-16 17:26:57 +1100

    Pure component-router example without React Bindings

--- src/example/App/index.js
+++ src/example/App/index.js
@@ -1,82 +1,19 @@
 import React from 'react';
-import {locationHistory as location} from '../..';
-import Url from './Bindings/Url';
-import ComponentRouteContainer from './Bindings/ComponentRouteContainer';
-import componentRouteHandler from './Bindings/componentRouteHandler';
-import pathnameRouteHandler from './Bindings/pathnameRouteHandler';
+import {locationHistory as location, store, actions, href, isActive} from '../..';
 import css from './App.css';
 
+const Header = React.createClass({
+  propTypes: {
+    links: React.PropTypes.array
+  },
 
-const ComponentHeader = React.createClass({
-  render() {
-    return (
-      <header className={css.header}>
-        <nav className={css.nav}>
-          <ul>
-            <li>
-              <Url query={{page: 'quickstart'}} className={css.tab}>Quickstart</Url>
-            </li>
-            <li>
-              <Url query={{page: 'foobar'}} className={css.tab}>FooBar</Url>
-            </li>
-            <li className={css.github}>
-              <a
-                href="https://github.com/in-flux/component-router"
-                target="_blank"
-                className={css.tab}>GitHub</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-});
+  getInitialState() {
+    return store.getState();
+  },
 
 
-const PathnameHeader = React.createClass({
-  render() {
-    return (
-      <header className={css.header}>
-        <nav className={css.nav}>
-          <ul>
-            <li>
-              <Url href="/quickstart" className={css.tab}>Quickstart</Url>
-            </li>
-            <li>
-              <Url href="/foobar" className={css.tab}>FooBar</Url>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-});
-
-
-const NotFound = () => <h1>Not Found.</h1>;
-
-
-const ComponentRouteHandler = componentRouteHandler({
-  namespace: 'page',
-  defaultValue: 'quickstart',
-  notFound: NotFound
-})({
-  quickstart: () => <p>Quickstart</p>,
-  foobar: () => <p>FooBar</p>
-});
-
-
-const PathnameRouteHandler = pathnameRouteHandler({
-  notFound: NotFound
-})({
-  '/quickstart': () => <p>Quickstart</p>,
-  '/foobar': () => <p>FooBar</p>
-});
-
-
-const App = React.createClass({
   componentDidMount() {
-    this.unsubscribe = location();
+    this.unsubscribe = store.subscribe(this.onChange);
   },
 
 
@@ -84,24 +21,122 @@ const App = React.createClass({
     this.unsubscribe();
   },
 
+
+  onChange() {
+    this.replaceState(store.getState());
+  },
+
+
+  navigateTo(params) {
+    return event => {
+      event.preventDefault();
+      store.dispatch(actions.navigateTo(params));
+    }
+  },
+
+
+  render() {
+    const {links} = this.props;
+
+    const makeLink = params => {
+      const {query, pathname} = this.state;
+      const mergedParams = {
+        pathname,
+        ...params,
+        query: {...query, ...params.query}
+      };
+      return (
+        <a className={css.tab}
+          href={href(mergedParams)}
+          data-active={isActive(mergedParams)}
+          onClick={this.navigateTo(mergedParams)}>
+          {JSON.stringify(mergedParams)}
+        </a>
+      )
+    };
+
+    return (
+      <header className={css.header}>
+        <nav className={css.nav}>
+          <ul>
+            {links.map(params => <li key={JSON.stringify(params)}>{makeLink(params)}</li>)}
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+});
+
+
+const Component = React.createClass({
+  getInitialState() {
+    return store.getState();
+  },
+
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(this.onChange);
+  },
+
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  },
+
+
+  onChange() {
+    this.replaceState(store.getState());
+  },
+
+
   render() {
     return (
+      <div>
+        <p>Pathname: {this.state.pathname}</p>
+        <p>Query: {JSON.stringify(this.state.query)}</p>
+        <p>Defaults: {JSON.stringify(this.state.defaultParams)}</p>
+        <p>Cleaned query (no defaults): {JSON.stringify(this.state.cleanQuery)}</p>
+      </div>
+    );
+  }
+});
+
+const links = [
+  {pathname: '/quickstart'},
+  {pathname: '/foobar'},
+  {query: {page: 'quickstart'}},
+  {query: {page: 'foobar'}}
+];
+
+
+const App = React.createClass({
+  componentDidMount() {
+    this.unsubscribe = location();
+
+    links
+      .filter(({pathname}) => pathname)
+      .forEach(({pathname}) => store.dispatch(actions.addRoute(pathname)));
+
+    store.dispatch(actions.addDefaultParam('page', 'quickstart'));
+  },
+
+
+  componentWillUnmount() {
+    this.unsubscribe();
+
+    links
+      .filter(({pathname}) => pathname)
+      .forEach(({pathname}) => store.dispatch(actions.removeRoute(pathname)));
+
+    store.dispatch(actions.removeParam('page'));
+  },
+
+  render() {
+
+    return (
       <div className={css.app}>
-        <ComponentRouteContainer>
-          {({query, currentRoute: {route, params}}) => (
-            <div>
-              <h1>ComponentRouteHandler</h1>
-              <ComponentHeader />
-              <ComponentRouteHandler params={query} />
-
-              <h1>PathnameRouteHandler</h1>
-              <PathnameHeader />
-              <PathnameRouteHandler route={route} params={params} />
-            </div>
-          )}
-        </ComponentRouteContainer>
-
-
+        <Header links={links} />
+        <Component />
       </div>
     );
   }
