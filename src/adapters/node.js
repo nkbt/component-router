eec58c181e13d634400e49c9004f98e3d31fb32c
Nik Butenko <nik@butenko.me>
2016-09-08 17:59:54 +1000

    Fix node adapter

--- src/adapters/node.js
+++ src/adapters/node.js
@@ -1,13 +1,12 @@
 import {restoreLocation, addRoute} from '../actions';
+import {searchToQuery} from '../codec';
 
 
 export const locationNode = ({store, routes}) => ({url}) => {
   const [pathname, search = ''] = url.split('?');
+  const query = searchToQuery(`?${search}`);
 
-  store.dispatch(restoreLocation({
-    pathname,
-    search: search.length > 0 ? `?${search}` : ''
-  }));
+  store.dispatch(restoreLocation({pathname, query}));
 
   routes.forEach(route => store.dispatch(addRoute(route)));
 };
